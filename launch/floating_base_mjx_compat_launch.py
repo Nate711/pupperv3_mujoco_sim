76e76264092080f77b3f0799ddcc88def2dfeb09
nathan kau <nathankau@gmail.com>
2024-02-24 00:04:04 +0900

    add floating mjx compat

---
+++ launch/floating_base_mjx_compat_launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory("pupper_mujoco_sim")
+    return LaunchDescription(
+        [
+            Node(
+                package="pupper_mujoco_sim",
+                executable="mujoco_interactive_test",
+                name="mujoco_interactive_test",
+                output="screen",
+                emulate_tty=True,
+                parameters=[
+                    {
+                        "publish_rate": 500.0,
+                        "model_xml": share_dir
+                        + "/src/urdf/pupper_v3_2_floating_base_mjx_compat.xml",
+                        "timestep": 0.001,
+                    }
+                ],
+            )
+        ]
+    )
